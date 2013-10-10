68be5396baad70a8286dd8aa02ebb86393f017bf
ibeaumont <ibeaumont@zubirimanteo.com>
2013-10-10 16:28:53 +0200

    con php

---
+++ componentes/header.php
@@ -0,0 +1,10 @@
+<div class="row">
+	<div class="col col-lg-9">
+	<!--imagen de los cubos-->
+		<img src="img/layout_set_logo.png" alt="zubiri" class="img-responsive"/>
+	</div>
+	<div class="col col-lg-3">
+	<!--imagen del logo-->
+		<img src="img/logoZubiri.png" alt="zubiri" class="img-responsive"/>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -1,60 +1,27 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>Zubiri Manteo MegaInsti-- Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
+		<link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">
+   
   </head>
-  <body>
-  	<div class="container">
-  	<div class="row">
-  		<div class="col-sm-12">
-  			<img src="img/layout_set_logo.png" alt="zubiri" />
-  		</div><!--end imagen zubiri -->
-  	</div>
-  	<div class="row">
-  		<form class="form-horizontal col-sm-8" role="form">
-		  <div class="form-group">
-		    <label for="inputEmail1" class="col-sm-6 control-label">usuario:</label>
-		    <div class="col-sm-6">
-		      <input type="email" class="form-control" id="txtUsuario" placeholder="tu correo">
-		    </div>
-		  </div>
-		  <div class="form-group">
-		    <label for="inputPassword1" class="col-sm-6 control-label">Password</label>
-		    <div class="col-sm-6">
-		      <input type="password" class="form-control" id="txtPassword" placeholder="Password">
-		    </div>
-		  </div>
-		  <div class="form-group">
-		    <div class="col-sm-offset-6 col-sm-6">
-		      <div class="checkbox">
-		        <label>
-		          <input type="checkbox" id="chkRecordar"> Recordar
-		        </label>
-		      </div>
-		    </div>
-		  </div>
-		  <div class="form-group">
-		    <div class="col-sm-offset-6 col-sm-6">
-		      <button type="button" class="btn btn-primary btn-lg btn-block">Aceptar</button>
-		    </div>
-		  </div>
-		</form><!--end formulario-->
-	<div class="col-sm-4">
-		<img src="img/logoZubiri.png" alt="zubiri" />
-	</div><!-- end logo-->
-</div><!--row del formulario+logo-->
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <body id="home">
+		<section class="container">
+			<div class="content row">
+          <?php include "componentes/header.php"; ?>
+				<section class="main col col-lg-8">
+          <?php include "componentes/main.php"; ?>
+				</section><!-- main -->
+				<section class="sidebar col col-lg-4">
+          <?php include "componentes/noticias.php"; ?>
+				</section><!-- sidebar -->
+			</div><!-- content -->
+    <?php include "componentes/footer.php"; ?>
+		</section><!-- container -->
+
     <script src="assets/js/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="js/app.js"></script>
   </body>
 </html>
