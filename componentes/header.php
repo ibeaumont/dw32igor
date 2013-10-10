20d742c152e86d70a31474c106bcbd137f497fd5
ibeaumont <ibeaumont@zubirimanteo.com>
2013-10-10 16:46:24 +0200

    con php

--- componentes/header.php
+++ componentes/header.php
@@ -1,10 +1,36 @@
 <div class="row">
-	<div class="col col-lg-9">
+	<div class="col col-lg-10">
 	<!--imagen de los cubos-->
 		<img src="img/layout_set_logo.png" alt="zubiri" class="img-responsive"/>
 	</div>
-	<div class="col col-lg-3">
+	<div class="col col-lg-2">
 	<!--imagen del logo-->
 		<img src="img/logoZubiri.png" alt="zubiri" class="img-responsive"/>
 	</div>
+	<!-- inicio NAVBAR -->
+	<div class="bs-example">
+      <nav class="navbar navbar-inverse" role="navigation">
+        
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex8-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Zubiri Manteo</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex8-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Inicio</a></li>
+            <li><a href="formacion.php">Formacion</a></li>
+            <li><a href="proyectos.php">Proyectos</a></li>
+            <li><a href="proyectos.php">Mapa</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </nav>
+    </div>
+    <!-- final NAVBAR -->
 </div>
