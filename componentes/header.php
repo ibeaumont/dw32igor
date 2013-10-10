75081f1e7ef960f3ff10d3b062ebe70d6cb6f52d
ibeaumont <ibeaumont@zubirimanteo.com>
2013-10-10 17:21:38 +0200

    con php

--- componentes/header.php
+++ componentes/header.php
@@ -27,7 +27,18 @@
         <div class="collapse navbar-collapse navbar-ex8-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
-            <li><a href="formacion.php"><span class="glyphicon glyphicon-book"></span> Formacion</a></li>
+        	<div class="btn-group">
+		        <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-book"></span> Formacion</a></button>
+		        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>
+		        <ul class="dropdown-menu" role="menu">
+		          <li><a href="#">Informatica</a></li>
+		          <li><a href="#">Turismo</a></li>
+		          <li><a href="#">Administrativo</a></li>
+		          <li class="divider"></li>
+		          <li><a href="#">Otros</a></li>
+		        </ul>
+	      	</div>
+		    <li><a href="formacion.php"><span class="glyphicon glyphicon-book"></span> Formacion</a></li>
             <li><a href="proyectos.php"><span class="glyphicon glyphicon-plane"></span> Proyectos</a></li>
             <li><a href="proyectos.php"><span class="glyphicon glyphicon-picture"></span> Mapa</a></li>
           </ul>
