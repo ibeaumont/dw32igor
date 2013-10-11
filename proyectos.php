23069a022196d0ae9884b093bbc42775c2ba2143
ibeaumont <ibeaumont@zubirimanteo.com>
2013-10-11 19:17:42 +0200

    sidebar noticias

--- proyectos.php
+++ proyectos.php
@@ -19,7 +19,73 @@
           cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
           proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <div>
+          <div class="col-sm-6 col-md-3">
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+          </div>
+          <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
             <div class="thumbnail">
                 <img data-src="img/besTrain.png" alt="BesTrain">
                 <div class="caption">
@@ -72,6 +138,58 @@
                   <p><a href="#" class="btn btn-primary">Mas info...</a></p>
                 </div>
             </div>
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>            
           </div>
           </div><!--fin proyectos -->
 				
