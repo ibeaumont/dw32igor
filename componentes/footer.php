d6fd2a5c98b43247b2f827e53ba3a25c587dcadd
ibeaumont <ibeaumont@zubirimanteo.com>
2013-10-11 18:53:52 +0200

    sidebar noticias

--- componentes/footer.php
+++ componentes/footer.php
@@ -1,2 +1,5 @@
 
 <img src="img/footer.png" alt="footer" class="img-responsive"/>
+<p>© IES Zubiri-Manteo BHI 
+Telefono: 943278700 | Fax: 943282477 | Telefono: 943326088 | bhi@zubirimanteo.com 
+</p>
