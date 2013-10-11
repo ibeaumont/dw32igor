3ec1b78daa1142495ec36eda3b95e65477daa1c5
ibeaumont <ibeaumont@zubirimanteo.com>
2013-10-11 19:10:51 +0200

    sidebar noticias

--- proyectos.php
+++ proyectos.php
@@ -9,13 +9,72 @@
   <body id="proyectos">
 		<section class="container">
 			<div class="content row">
-          <?php include "componentes/header.php"; ?>
-				<section class="main col col-lg-8">
-          <?php include "componentes/main.php"; ?>
-				</section><!-- main -->
-				<section class="sidebar col col-lg-4">
-          <?php include "componentes/noticias.php"; ?>
-				</section><!-- sidebar -->
+         <?php include "componentes/header.php"; ?>
+  			 <div class="row">
+          <h3>Proyectos</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div class="col-sm-6 col-md-3">
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+            <div class="thumbnail">
+                <img data-src="img/besTrain.png" alt="BesTrain">
+                <div class="caption">
+                  <h3>BesTrain</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
+                </div>
+            </div>
+          </div>
+          </div><!--fin proyectos -->
+				
 			</div><!-- content -->
     <?php include "componentes/footer.php"; ?>
 		</section><!-- container -->
