8147c48a02212994825f30ea250be7470fc2bf1a
ibeaumont <ibeaumont@zubirimanteo.com>
2013-10-14 19:29:38 +0200

    con php

--- componentes/menuFormacion.php
+++ componentes/menuFormacion.php
@@ -1,13 +1,9 @@
 <!--menu para la navegacion en formacion -->
-<div class="tabbable tabs-left">
-  <ul class="nav nav-tabs tabs-left">
+  <ul class="nav nav-pills nav-stacked">
     <li class="active"><a href="#infor" data-toggle="tab">Informatica</a></li>
     <li><a href="#turismo" data-toggle="tab">Turismo</a></li>
     <li><a href="#administrativo" data-toggle="tab">Administrativo</a></li>
     <li><a href="#comercio" data-toggle="tab">Comercio</a></li>
     <li><a href="#otros" data-toggle="tab">Otros</a></li>
   </ul>
-  <div class="tab-content">
- 
-  </div>
-</div>
+  
