d966fd9efa8c08f1b70ca37662e30d28942ee2cc
ibeaumont <ibeaumont@zubirimanteo.com>
2013-10-14 19:51:59 +0200

    con php

--- componentes/mainFormacion.php
+++ componentes/mainFormacion.php
@@ -1,6 +1,6 @@
 <div class="tab-content">
     <div class="tab-pane active" id="infor">
-      <h3>Informatica</h3>
+      <a name="#informatica"><h3>Informatica</h3></a>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
