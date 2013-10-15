3e7919cfd90bf7b8405be268a1a8913af630532d
ibeaumont <ibeaumont@zubirimanteo.com>
2013-10-15 17:55:03 +0200

    menu con todo a la vista

--- componentes/mainFormacion.php
+++ componentes/mainFormacion.php
@@ -1,5 +1,5 @@
-<div class="tab-content">
-    <div class="tab-pane active" id="infor">
+<div>
+    <div id="infor">
       <a name="#informatica"><h3>Informatica</h3></a>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -9,7 +9,7 @@
       proident,sunt in culpa qui officia deserunt mollit anim id est laborum.
   	 </p>
     </div>
-    <div class="tab-pane" id="turismo">
+    <div id="turismo">
       <h3>Turismo</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -19,7 +19,7 @@
       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   	 </p>
     </div>
-    <div class="tab-pane" id="administrativo">
+    <div id="administrativo">
       <h3>Administrativo</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -29,7 +29,7 @@
       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   	 </p>
     </div>
-    <div class="tab-pane" id="comercio">
+    <div id="comercio">
       <h3>Comercio</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -39,7 +39,7 @@
       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   	 </p>
     </div>
-    <div class="tab-pane" id="otros">
+    <div id="otros">
       <h3>Otros</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
