10ffdf95ddfd023235d369c0c5fb194656544479
ibeaumont <ibeaumont@zubirimanteo.com>
2013-10-15 18:17:18 +0200

    menu con todo a la vista

--- componentes/menuFormacion.php
+++ componentes/menuFormacion.php
@@ -1,5 +1,5 @@
 <!--menu para la navegacion en formacion -->
-  <ul class="nav nav-pills nav-stacked" id="navFormacion">
+  <ul class="nav nav-pills nav-stacked" id="navFormacion navbar-static-top">
     <li class="active"><a href="#informatica">Informatica</a></li>
     <li><a href="#turismo" >Turismo</a></li>
     <li><a href="#administrativo">Administrativo</a></li>
