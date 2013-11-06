476553566f7c6cec1687fcea369b0b17b7fc09ea
ibeaumont <ibeaumont@zubirimanteo.com>
2013-11-06 23:26:45 +0100

    kk

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>Zubiri Manteo MegaInsti-- Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">
-   
+   <?php header('Access-Control-Allow-Origin: *'); ?>
   </head>
   <body id="inicio">
 		<section class="container">
