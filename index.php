7f62e545df96ed3a4d2891d37655e4bd07f98500
ibeaumont <ibeaumont@zubirimanteo.com>
2013-11-06 19:06:00 +0100

    lg-<sm

--- index.php
+++ index.php
@@ -33,8 +33,25 @@
     <script src="assets/js/bootstrap.js"></script>
     <script src="js/app.js"></script>
     <script>
+
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: "zurriola",
+    tagmode: "any",
+    format: "json"
+  })
+    .done(function( data ) {
+      console.log(data);
+      /*$.each( data.items, function( i, item ) {
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+        if ( i === 3 ) {
+          return false;
+        }*/
+      });
+    });
+
     
-    var ajaxConn=$.ajax({
+  /*  var ajaxConn=$.ajax({
         url : '/data/people.json',
         type : 'GET',
         dataType : 'json'
@@ -47,7 +64,7 @@
             $(lista).append('<li>'+el.name+'</li>')
           })
             
-      })
+      })*/
       
     </script>
   </body>
