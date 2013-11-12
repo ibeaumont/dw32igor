3dc142227b6c3c7637910752907f1a85116634ca
ibeaumont <ibeaumont@zubirimanteo.com>
2013-11-12 17:57:32 +0100

    lg-<sm

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
         $('#accordion .panel-default .panel-collapse').eq(id).attr('id','collapse'+id)
       })
       
-      $("#accordion .collapse").collapse()
+      //$("#accordion .collapse").collapse()
 
       //modificar los img para que se cojan de la página de zubiri
       $('#accordion .panel-body img').each(function(idx,el){$(el).attr('src','http://www.zubirimanteo.com'+$(el).attr('src'))})
