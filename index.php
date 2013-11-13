cd84f219b1ab16ee2ed04453c55660e60ebe0082
ibeaumont <ibeaumont@zubirimanteo.com>
2013-11-14 00:06:51 +0100

    kk

--- index.php
+++ index.php
@@ -43,13 +43,13 @@ var url_rockStar='http://www.salarockstar.com/articles.html';
    var yql_url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22" + encodeURIComponent(url_rockStar) + "%22%20and%0A%20%20%20%20%20%20xpath%3D'%2F%2Ftitle'&format=json&callback=?";
  
  $.ajax({
-                    url: url_rockStar,
-                    datatype: "html",
-                    success: function (data) {
-                        datos=data;
-                        alert('Load was performed.');
-                    }
-                });
+    url:url_rockStar,
+    type:'GET',
+    dataType:'JSONP',
+    success: function(data){
+        $('body').append( "Name: " + data );
+    }
+});
 
 
 //mostrar las noticias del rss de zubiri manteo en el accordion
