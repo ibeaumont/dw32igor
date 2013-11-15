2ca4a6b7f6a08b87fc193db18060efe4c9c106a0
ibeaumont <ibeaumont@zubirimanteo.com>
2013-11-15 18:35:08 +0100

    lg-<sm

--- eventos.php
+++ eventos.php
@@ -12,89 +12,10 @@
          <?php include "componentes/header.php"; ?>
   			 
       </div>
-      <div class="row">
-          <div class="col-md-4">
-            <div class="thumbnail">
-                <img src="img/besTrain.png" alt="BesTrain">
-                <div class="caption">
-                  <h3>BesTrain</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
-                </div>
-            </div><!-- fin thunb-->
-          </div>
-          <div class="col-md-4">
-            <div class="thumbnail">
-                <img src="img/eleBasque.jpg" alt="BesTrain">
-                <div class="caption">
-                  <h3>EleBasque</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
-                </div>
-            </div><!-- fin thunb-->
-          </div>
-          <div class="col-md-4" >
-
-            <div class="thumbnail">
-                <img src="img/menoresOsi.jpg" alt="Menores OSI">
-                <div class="caption">
-                  <h3>MenoresOSI</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
-                </div>
-            </div><!-- fin thunb-->
-          </div>
-        </div><!-- fin row-->
-        <div class="row">
-          <div class="col-md-4">
-
-            <div class="thumbnail">
-                <img src="img/sareSozialak.jpg" alt="Sare sozialak">
-                <div class="caption">
-                  <h3>Sare sozialak</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
-                </div>
-            </div><!-- fin thunb-->
-          </div>
-          <div class="col-md-4">
-            <div class="thumbnail">
-                <img src="img/amadeus.jpg" alt="Amadeus">
-                <div class="caption">
-                  <h3>Amadeus</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                  <p><a href="#" class="btn btn-primary">Mas info...</a></p>
-                </div>
-            </div><!-- fin thunb-->
-          </div>
+<!--aqui vamos a introducir los thumbnails -->
 
 
-  	</div><!--row thumbnails-->
+  	
     <?php include "componentes/footer.php"; ?>
 		</section><!-- container -->
 
@@ -113,9 +34,30 @@ var url_rockStar='http://www.salarockstar.com/articles.html';
             datos=$(data.contents).find('.list1')
             images=$(datos).find('img').each(function(){
             $(this).attr('src','http://www.salarockstar.com/'+$(this).attr('src'))
+            for(i=0;i<images.length;i++){
+              if (i%0==0){
+                //escribir una nueva linea
+                $('container').append('<div class="row">')
+              }
+              //añadir el thumbnail con los datos
+              $('container row').last()
+                 .append(showThmbnail('kk',$(images[i]).attr('src'),'url'));
+)            }
 
           })
 });
+ function showThumbnail(tit,img,url){
+  var cod='<div class="col-md-4">'+
+              '<div class="thumbnail">'+
+              '<img src="+'+img+'" alt="BesTrain">'+
+                  '<div class="caption">'+
+                  '<h3>'+tit+'</h3>'+
+                  '<p></p>'+
+                  '<p><a href="'+url+'" class="btn btn-primary">Mas info...</a></p>'+
+                '</div>'+
+            '</div>';
+  return cod;
+ }
     </script>
   </body>
 </html>
