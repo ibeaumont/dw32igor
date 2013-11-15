47e0efb84f17a14da3556e21718129fbabec6d57
ibeaumont <ibeaumont@zubirimanteo.com>
2013-11-15 22:57:51 +0100

    kk

--- mapa.php
+++ mapa.php
@@ -11,7 +11,7 @@
 			<div class="content row">
           <?php include "componentes/header.php"; ?>
 				<section class="main col col-lg-8">
-          <?php include "componentes/main.php"; ?>
+          <?php include "componentes/map.php"; ?>
 				</section><!-- main -->
 				<section class="sidebar col col-lg-4">
           <?php include "componentes/noticias.php"; ?>
@@ -23,5 +23,20 @@
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.js"></script>
     <script src="js/app.js"></script>
+    <script type="text/javascript"
+      src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCKCfIwvBuXvsA2HdVQTb1Ny-zLQ1jjXjw
+&sensor=false">
+    </script>
+    <script type="text/javascript">
+      function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+      }
+    </script>
   </body>
 </html>
