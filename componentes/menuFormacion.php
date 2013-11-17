e93f494cebb6756280d9054d93340954a80b5938
ibeaumont <ibeaumont@zubirimanteo.com>
2013-11-17 23:38:51 +0100

    kk

--- componentes/menuFormacion.php
+++ componentes/menuFormacion.php
@@ -1,6 +1,6 @@
 <!--menu para la navegacion en formacion -->
-<div id="navFormacion">
-  <ul class="nav nav-pills nav-stacked affix">
+<div id="navFormacion affix">
+  <ul class="nav nav-pills nav-stacked">
     <li class="active"><a href="#informatica">Informatica</a></li>
     <li><a href="#turismo" >Turismo</a></li>
     <li><a href="#administrativo">Administrativo</a></li>
